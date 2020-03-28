72637528fed50d7de935bc17963674fd8e810562
ian-1 <ian.walton@portmoor.com>
2020-03-28 14:40:42 +0000

    Add #Draw & #line to Draw

--- lib/draw.rb
+++ lib/draw.rb
@@ -1,1 +1,20 @@
+require 'tk'
 
+# Draws shapes to canvas using TkCanvas
+class Draw
+  def initialize
+    @instructions = []
+  end
+
+  def draw
+    canvas = TkCanvas.new(width: 800, height: 600)
+    canvas.pack
+    @instructions.each { |instruction| eval(instruction) }
+    canvas.mainloop
+  end
+
+  def line(x_start, y_start, x_end, y_end)
+    co_ordinates = "#{x_start}, #{y_start}, #{x_end}, #{y_end}"
+    @instructions << "TkcLine.new(canvas, #{co_ordinates})"
+  end
+end
--- test/TkCanvas.rb
+++ test/TkCanvas.rb
@@ -4,6 +4,5 @@ require 'tk'
 
 canvas = TkCanvas.new(width: 800, height: 600)
 canvas.pack
-TkcLine.new(canvas, 100, 200, 300, 400)
-TkcLine.new(canvas, 100, 100, 200, 200)
+TkcLine.new(canvas, 100, 100, 200, 100)
 canvas.mainloop
